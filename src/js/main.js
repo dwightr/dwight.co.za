706090546015a7f761cd879210c23e927b6e1a9b
_codefreak <dramokhola@gmail.com>
2018-05-11 17:26:26 +0200

    Changes updats and fixes

--- src/js/main.js
+++ src/js/main.js
@@ -124,7 +124,7 @@ TxtRotate.prototype.tick = function () {
     } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
-        delta = 500;
+        delta = 200;
     }
 
     setTimeout(function () {
