450f1c05e06863a120d91be6d341f14ce81cf6f3
_codefreak <dramokhola@gmail.com>
2018-07-06 11:00:58 +0200

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -91,6 +91,29 @@ $(function () {
         }, 6000);
     });
 
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
 });
 
 var TxtRotate = function (el, toRotate, period) {
@@ -208,3 +231,18 @@ $(function () {
         }, 600);
     });
 });
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.nav-items a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav-items a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else {
+            currLink.removeClass("active");
+        }
+    });
+}
